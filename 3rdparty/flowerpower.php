6529c0a87e1389acf183452d7ec1352fbb60d62b
Cédric Dupont <lunarok@gmail.com>
2016-10-26 21:42:51 +0200

    api

--- 3rdparty/flowerpower.php
+++ 3rdparty/flowerpower.php
@@ -23,20 +23,9 @@ class fpParrotAPI
 			//echo "--expired--";
 		}
 		if ($this->access_token == '') {
-			//echo "--no access token--";
-			if (strlen($this->refresh_token) > 1) {
-				//echo "--refresh token ok--";
-		   		// on peut juste rafraichir le token
-		    	$grant_type = 'refresh_token';
-		    	$postdata = 'grant_type='.$grant_type.'&refresh_token='.$this->refresh_token;
-				$fields=2;
-			}else{
-			    // 1ère utilisation aprés obtention du code
-			    //echo "--refresh token nok--";
-			    $grant_type = 'password';
-			    $postdata = 'grant_type='.$grant_type.'&username='.urlencode($this->username).'&password='.urlencode($this->password).'&client_id='.$this->client_id.'&client_secret='.$this->client_secret;
-				$fields=5;
-		  	}
+			$grant_type = 'password';
+			$postdata = 'grant_type='.$grant_type.'&username='.urlencode($this->username).'&password='.urlencode($this->password).'&client_id='.$this->client_id.'&client_secret='.$this->client_secret;
+			$fields=5;
 			$ch = curl_init();
 			curl_setopt($ch,CURLOPT_URL, $this->api_url.'user/v1/authenticate');
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -51,9 +40,8 @@ class fpParrotAPI
 			}*/
 			//var_dump($params);
 			// on sauvegarde l'access_token et le refresh_token pour les authentifications suivantes
-			if (isset($params->refresh_token)) {
+			if (isset($params->access_token)) {
 				$this->access_token=$params->access_token;
-				$this->refresh_token=$params->refresh_token;
 				$this->expire_time=time()+$params->expires_in;
 			}else if ($this->access_token == '') {
 				die("Erreur lors de l'authentification");
@@ -67,7 +55,7 @@ class fpParrotAPI
     {
 		//open connection
 		$ch = curl_init();
-		curl_setopt($ch,CURLOPT_URL, $this->api_url.$request."?access_token=".$this->access_token);
+		curl_setopt($ch,CURLOPT_URL, $this->api_url.$request);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array( 'Authorization: Bearer ' . $this->access_token ));
 		$result = curl_exec($ch);
